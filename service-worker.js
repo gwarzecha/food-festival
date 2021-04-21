5f2c6d8b39078109656af4788e22a52aa04fa8de
gwarz <gmwarzecha@tutanota.com>
2021-04-20 21:08:45 -0700

    added offline functionality using service workers

--- service-worker.js
+++ service-worker.js
@@ -15,6 +15,25 @@ const FILES_TO_CACHE = [
   "./dist/schedule.bundle.js"
 ];
 
+// listens for fetch event and logs URL of the requested resource
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    // .match() determines if the resource exists in caches
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
 
 // service workers fun before the window obj is created, so self is used instead 
 //to instantiate listeners on the service worker. Self refers to the service worker object
@@ -30,19 +49,19 @@ self.addEventListener('install', function (e) {
 })
 
 // Delete outdated caches
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
   e.waitUntil(
-    caches.keys().then(function(keyList) {
+    caches.keys().then(function (keyList) {
       // `keyList` contains all cache names under your username.github.io
       // filter out ones that has this app prefix to create keeplist
-      let cacheKeeplist = keyList.filter(function(key) {
+      let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
       });
       // add current cache name to keeplist
       cacheKeeplist.push(CACHE_NAME);
 
       return Promise.all(
-        keyList.map(function(key, i) {
+        keyList.map(function (key, i) {
           if (cacheKeeplist.indexOf(key) === -1) {
             console.log('deleting cache : ' + keyList[i]);
             return caches.delete(keyList[i]);
