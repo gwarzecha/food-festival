dc00efc9f335209b47691348dda4d3fb195e431f
gwarz <gmwarzecha@tutanota.com>
2021-04-20 19:18:32 -0700

    optimized imgs using file-loader and image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,43 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 // main configuration object-- options are written in this object that tell webpack what to do
 module.exports = {
   // entry point is the root of the bundle and beginning of the dependency graph
-  entry: './assets/js/script.js',
+  entry: { 
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
+  },
   // put the bundled code into the dist folder
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    // the name of each attribute in the entry obj will be used in place of [name] in each bundle.js file created
+    filename: '[name].bundle.js',
+    path: __dirname + '/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            // file-loader processes the imgs
+            loader: "file-loader",
+            // HOW DOES IT DO THIS?!?!?!?!?!?
+            options: {
+              name (file) {
+                return "[path][name].[ext]"
+              },
+              publicPath: function(url) {
+                return url.replace("../", "/assets/")
+              }
+            }  
+          },
+          {
+            // package for img optimization
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new webpack.ProvidePlugin({
