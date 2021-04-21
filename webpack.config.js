8030ca846b1c49025542f0ed76ea05acedb00e82
gwarz <gmwarzecha@tutanota.com>
2021-04-21 15:57:38 -0700

    installed webpack-pwa-manifest and configured plugin to make app a pwa

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 // ensures that webpack's methods and properties are being brought into the config file
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 
 // main configuration object-- options are written in this object that tell webpack what to do
@@ -53,8 +53,28 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
+    }),
+    // 'new' means a constructor function is invoked
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      // a unique fingerprint should not be created with each new manifest
+      fingerprints: false,
+      inject: false,
+      icons: [{
+        src: path.resolve("assets/img/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
     })
   ],
   // provides the mode which the code should be run in 
   mode: 'development'
 };
+
+
+
